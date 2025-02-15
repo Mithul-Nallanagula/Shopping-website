c7a747a16943eb344ab903b77940ca48cf060a55
mithul nallanagula <mithulnallanaula@gmail.com>
2025-02-15 22:35:58 +0530

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
   };
   return (
     <div className=' bg-white flex-wrap w-screen h-[80px] flex items-center justify-evenly max-sm:justify-between  '>
-        <img src='/src/assets/logo.png' className='flex w-[250px] cursor-pointer sm:w-[150px] max-sm:w-[100px] ' onClick={() => navigate("/")} ></img>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSspnibYzmgf7HITswAnkIu2w1WXExW-f37FyJzuj1uGVCJRgzRXmgKk5Ragqc-9hf2nE8&usqp=CAU' className='flex w-[250px]  cursor-pointer sm:w-[150px] max-sm:w-[100px] ' onClick={() => navigate("/")} ></img>
         <input type="text" placeholder='Browse Any Item' onChange={(e) => setQuery(e.target.value)} onKeyDown={handleKeyDown} value={query} className='max-sm:text-xs max-sm:h-[25px] h-[40px] w-[30vw] p-2 bg-gray-200 outline-none sm:w-[40vw]'></input>
         <div className='flex w-[10vw] h-[5vh] justify-between items-center md:mr-[3vw] max-sm:mr-1 max-sm:w-[30vw]'>
             <h2 className='text-2xl cursor-pointer xl:text-xl md:text-xl md:m-3 sm:text-[15px] max-sm:text-xs  ' onClick={() => navigate("/")} >Home</h2>
